bc75bc046bb01504c051a13cede78ad726bfc6a4
Khushboo Jhingran <khushboojh@gmail.com>
2018-06-28 09:01:22 +0530

    intial upload

---
+++ ConsoleApplication2/FlightDataProvider.cs
@@ -0,0 +1,97 @@
+﻿using FlightEngine;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace FlightEngine
+{
+    public class FlightDataProvider
+    {
+
+        internal static List<FlightRoute> GetRouteInformation()
+        {
+            var routes = new List<FlightRoute>();
+            var provider1Data = new FlightDataProvider().GetFlightRouteFromFile("provider1.txt", ',', "M/d/yyyy H:mm:ss");
+            var provider2Data = new FlightDataProvider().GetFlightRouteFromFile("provider2.txt", ',', "M-d-yyyy H:mm:ss");
+            var provider3Data = new FlightDataProvider().GetFlightRouteFromFile("provider3.txt", '|', "M/d/yyyy H:mm:ss");
+
+            routes.AddRange(provider1Data);
+            routes.AddRange(provider2Data);
+            routes.AddRange(provider3Data);
+
+            routes = GetDistinctRoutesWithLowestFare(routes).OrderBy(rR => rR.Rate.Value).ThenBy(rR => rR.DepartureTime).ToList();
+
+            return routes;
+        }
+
+        private static List<FlightRoute> GetDistinctRoutesWithLowestFare(List<FlightRoute> routes)
+        {
+            var distinctRoutes = new List<FlightRoute>();
+            routes.ForEach(r1 =>
+            {
+                var matchingRoute = distinctRoutes.FirstOrDefault(r2 => r1.Origin == r2.Origin && r1.DepartureTime == r2.DepartureTime && r1.Destination == r2.Destination && r1.DestinationTime == r2.DestinationTime);
+                if (matchingRoute == null)
+                {
+                    distinctRoutes.Add(r1);
+                    return;
+                }
+                if (matchingRoute.Rate.Value > r1.Rate.Value) //Assuming the currency is same always that is USD
+                    matchingRoute.Rate.Value = r1.Rate.Value;
+
+            });
+
+            return distinctRoutes;
+        }
+
+        private List<FlightRoute> GetFlightRouteFromFile(string path, char delimiter, string dateTimeFormat)
+        {
+            var dataRows = File.ReadAllLines(path).ToList();
+            var flightRoutes = new List<FlightRoute>();
+            foreach (var row in dataRows)
+            {
+                var columns = row.Split(new char[] { delimiter }, StringSplitOptions.RemoveEmptyEntries);
+                if (columns.Count() != 5)
+                {
+                    continue;
+                }
+                
+                var route = ParseFlightRoute(columns, dateTimeFormat);
+                if(route != null)
+                    flightRoutes.Add(route);
+            }
+
+            if(flightRoutes.Count == 0)
+                Console.WriteLine("Parsing exception");
+
+            return flightRoutes;
+        }
+
+        private FlightRoute ParseFlightRoute(string[] columns, string dateTimeFormat)
+        {
+            try
+            {
+                return new FlightRoute
+                {
+                    Origin = columns.ElementAt(0),
+                    DepartureTime = DateTime.ParseExact(columns.ElementAt(1), dateTimeFormat, CultureInfo.InvariantCulture),
+                    Destination = columns.ElementAt(2),
+                    DestinationTime = DateTime.ParseExact(columns.ElementAt(3), dateTimeFormat, CultureInfo.InvariantCulture),
+                    Rate = new Amount
+                    {
+                        Value = decimal.Parse(Regex.Match(columns.ElementAt(4), @"\d+").Value),
+                        Currency = "USD" //Need to properly pass the currency
+                    }
+                };
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+    }
+}
---
+++ ConsoleApplication2/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlightEngine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            _validate(args);
+            var origin = args[2];
+            var destination = args[4];
+
+            var providerRoutes = FlightDataProvider.GetRouteInformation();
+
+            var requiredRoutes = providerRoutes.Where(r => r.Origin.Equals(origin) && r.Destination.Equals(destination)).ToList();
+            if (requiredRoutes == null || requiredRoutes.Count == 0) Console.WriteLine("No Flights found for this route");
+            else
+                foreach (var route in requiredRoutes)
+                    Console.WriteLine($"{route.Origin} --> {route.Destination} ({route.DepartureTime} --> {route.DestinationTime}) - ${route.Rate.Value}");
+
+        }
+
+        private static void _validate(string[] args)
+        {
+            if (!args[0].Equals("$searchFlights"))
+            {
+                Console.WriteLine("Something doesnt seem right");
+                Environment.Exit(0);
+            }
+            if (args.Length != 5)
+            {
+                Console.WriteLine("Args length doesn't match the expected Argument length");
+                Environment.Exit(0);
+            }
+        }
+    }
+    public class FlightRoute
+    {
+        public string Origin { get; set; }
+        public string Destination { get; set; }
+        public DateTime DepartureTime { get; set; }
+        public DateTime DestinationTime { get; set; }
+        public Amount Rate { get; set; }
+    }
+
+    public class Amount
+    {
+        public string Currency { get; set; }
+        public decimal Value { get; set; }
+    }
+}
